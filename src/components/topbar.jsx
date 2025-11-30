6f7c62f5ec42a931b6be22e4624669c4958931cf
Lucas <109859872+iamdevlucas123@users.noreply.github.com>
2025-11-30 17:20:54 -0300

    Organizing the enviroment - First commit

---
+++ src/components/topbar.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+export default function TopBar () {
+    const [open, setOpen] = useState(false)
+
+    return (
+        <header>
+            <div>
+                <a href=""></a>
+            </div>
+        </header>
+    )
+}
